566ce6940a92688358fbf25676a81dfa8affcb4d
Gordana Minovska <ekogoca@gmail.com>
2016-04-17 13:32:17 -0700

    Classic ADO Assignment

---
+++ Default.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="ClassicADO.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <main>
+    <form id="form1" runat="server">
+    <div>
+        <h1>Authors and Books</h1>
+        <hr />
+        <p>Select an author from the list to see the 
+            books he/she wrote.</p>
+
+        <!--a couple of things are important in the drop down list:
+            for one, the AutoPostBack property needs to be set
+            to true. This makes it go back to the server everytime
+            you make a new selection in the list.
+            Also you need the OnSelectedIndexChanged method
+            to run code when you change the selection
+            -->
+
+        <asp:DropDownList 
+            ID="AuthorDropDownList" 
+            runat="server" 
+            AutoPostBack="true" 
+            OnSelectedIndexChanged="BooksDropDownList_SelectedIndexChanged"
+            CssClass="list">
+            <asp:ListItem Value="0">Select a book</asp:ListItem>
+        </asp:DropDownList>
+
+        <asp:GridView 
+            ID="BooksGridView" 
+            runat="server" EmptyDataText="No books found.">
+            <EmptyDataRowStyle BorderStyle="None" ForeColor="#CC3300" />
+        </asp:GridView>
+        <!--to display any error messages thrown from the code-->
+        <asp:Label ID="ErrorLabel" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+        </main>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+//add this
+using System.Data;
+
+public partial class _Default : System.Web.UI.Page
+{
+    //decare the grants and services class so it is available
+    //to all methods
+    AuthorsAndBooks gs = new AuthorsAndBooks();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //if it is not a postback--ie. if it is not
+        //a repost due to an action on the page
+        //call the FillDropDownList Method
+        if (!Page.IsPostBack)
+        {
+            FillDropDownList();
+        }
+    }
+    protected void BooksDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        //when the index of the selected item in the
+        //drop down box changes call the FillGridViewMethod
+        FillGridView();
+    }
+
+    protected void FillDropDownList()
+    {
+        //Declare the dataTabe
+        DataTable table = null; ;
+        try
+        {
+            //call the method in the class to fill the table
+             table= gs.GetAuthors();
+        }
+        catch(Exception ex)
+        {
+            ErrorLabel.Text=ex.Message;
+        }
+        //Attach the table as a datasource for the 
+        //drop down list
+        //assign the display and value fields
+        AuthorDropDownList.DataSource = table;
+        AuthorDropDownList.DataTextField = "AuthorName";
+        AuthorDropDownList.DataValueField = "AuthorKey";
+        AuthorDropDownList.DataBind();
+        AuthorDropDownList.Items.Insert(0, new ListItem("Select an author", "0"));        
+    }
+
+    protected void FillGridView()
+    {
+        //Get the value from the drop down list
+        //selected value
+        int authorKey = int.Parse(AuthorDropDownList.SelectedValue.ToString());
+        DataTable tbl = new DataTable();
+        try
+        {
+            //call the GetServiceGrants field and pass 
+            //it the service key
+            //tbl = gs.;
+            tbl = gs.GetBooks(authorKey);
+        }
+        catch (Exception ex)
+        {
+            ErrorLabel.Text = ex.Message;
+        }
+
+        //attach the table as a data source to the
+        //gridView
+        BooksGridView.DataSource = tbl;
+        BooksGridView.DataBind();
+    }
+}
